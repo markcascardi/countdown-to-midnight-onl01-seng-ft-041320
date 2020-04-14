7888a4ce4e3f329e7d27e4efa93a1d24adb0436d
Mark Cascardi <mcascardi@gmail.com>
2020-04-14 03:25:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,7 @@ def countdown(number)
   while number < 0
     puts "#{number} SECOND(S)!"
     number -= 1
+    number == 0 ? "HAPPY NEW YEAR!"
   end
-  if number == 0
-    "HAPPY NEW YEAR!"
-  end 
+  
 end
